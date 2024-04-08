6aade3de6b66e07dce88b7126d93f79761f34b34
Ashwani Sharma <107782712+DRxStrange@users.noreply.github.com>
2024-04-09 01:36:45 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (47.18%) - LeetSync

--- 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.cpp
+++ 1486-find-the-distance-value-between-two-arrays/find-the-distance-value-between-two-arrays.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    // void solve(vector<int> &b, int m, int a, int d, int &count){
-    //     int low = 0, high = m-1;
-    //     while(low<=high){
-    //         int mid = (low+high)/2;
-    //         int dis = abs(a-b[mid]);
-    //         if(dis <= d){
-    //             low = mid-1;
-    //             count++;
-    //         }else{
-    //             high = mid+1;
-    //         }
+    bool solve(vector<int> &b, int m, int a, int d){
+        int low = 0, high = m-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            int dis = abs(a-b[mid]);
+            if(dis <= d){
+                return false;
+            }else if(b[mid]< a){
+               low = mid+1;
+            }else{
+                high = mid-1;
+            }
             
-    //     }
-    //     return ;
-    // }
+        }
+        return true;
+    }
     int findTheDistanceValue(vector<int>& a, vector<int>& b, int d) {
          int n = a.size();
          int m = b.size();
@@ -22,16 +23,9 @@ public:
         \
          int count =0;
          for(int i =0;i<n;i++){
-             bool flag = false;
-             for(int j =0;j<m;j++){
-                if(abs(a[i]-b[j]) <= d){
-                   flag = true;
-                   break;
-                }
-             }
-             if(!flag){
-                count++;
-             }
+             if(solve(b,m,a[i],d))
+              count++;
+             
          }
          return count;
     }
