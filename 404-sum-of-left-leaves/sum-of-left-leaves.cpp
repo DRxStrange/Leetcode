c85323641ae3071d0ce692c1ae0e8e426933a62d
Ashwani Sharma <107782712+DRxStrange@users.noreply.github.com>
2024-04-14 10:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (81.87%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -11,17 +11,23 @@
  */
 class Solution {
 public:
+// checking for leaf node
 bool isLeaf(TreeNode *root){
     return !root->left && !root->right;
 }
 void preOrder(TreeNode *root, int &sum){
     if(root == NULL) return;
+    // root->left should be there
     if(root->left != NULL){
+        // check node is leaf or not.
         if(isLeaf(root->left) == true){
+            // if node left and leaf add in sum variable
         sum+=root->left->val;
         }
+        // go for further left call
         preOrder(root->left,sum);
     }
+    //  go for right call;
     preOrder(root->right,sum);
 }
     int sumOfLeftLeaves(TreeNode* root) {
