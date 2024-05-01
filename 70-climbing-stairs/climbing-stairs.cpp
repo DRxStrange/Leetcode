69c7da4116c39c5f970da885f2775141a7fb73fa
Ashwani Sharma <107782712+DRxStrange@users.noreply.github.com>
2024-05-02 01:34:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (50.27%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -6,8 +6,20 @@ public:
         if(dp[n]!=-1) return dp[n];
         return dp[n] = solve(n-1,dp) + solve(n-2,dp);
     }
+
     int climbStairs(int n) {
-       vector<int> dp(n+1,-1);
-       return solve(n,dp);
+        /*
+            Recursion + Memoization
+            vector<int> dp(n+1,-1);
+            return solve(n,dp);
+         */
+
+       vector<int> dp(n+1,0);
+       dp[0] = 1;
+       dp[1] = 1;
+       for(int i =2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+       }
+       return dp[n];
     }
 };
