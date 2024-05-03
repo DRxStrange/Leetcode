7d463b828b487f139f7f763893c9c327869d2a9f
Ashwani Sharma <107782712+DRxStrange@users.noreply.github.com>
2024-05-03 17:33:45 +0530

    Time: 4 ms (10.03%) | Memory: 7.1 MB (57.30%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -8,8 +8,8 @@ public:
          for(int i=2;i<=n;i++){
              for(int x = 1;x<i;x++){
                 if(i%x==0 && !dp[i-x]){
-                    dp[i]= 1;
-                    break;
+                    dp[i]= true;
+                   
                 }
              }
          }
