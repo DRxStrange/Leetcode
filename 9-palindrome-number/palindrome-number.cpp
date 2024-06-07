a88c0d39bbe6d0aacf49827bc7b21710669cf7b3
Ashwani Sharma <107782712+DRxStrange@users.noreply.github.com>
2024-06-07 08:42:43 +0530

    Time: 14 ms (22.48%) | Memory: 8.2 MB (78.88%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
+bool check(int num){
+    if(num > INT_MAX /10 || num < INT_MIN/10) return true;
+    return false;
+}
     bool isPalindrome(int x) {
-        if(x<0)return false;
-        long long rev = 0;
-        long long front = x;
-        while(x!=0){
-          long long rem = x%10;
+        if(x < 0) return false;
+         int num = x;
+         int reverseNumber = 0;
+         while(x!=0){
+            int digit = x%10;
+            if(check(reverseNumber)) return false;
+            reverseNumber = reverseNumber * 10 + digit;
             x = x/10;
-            rev = rev*10 + rem;
-        }
-         cout<<rev;
-        if(front == rev) return true;
-        return false;
+         }
+         if(reverseNumber == num) return true;
+         return false;
     }
 };
